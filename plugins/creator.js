d14a9d31f7a6b010e006af18da07669b7a456323
Vv <hhbb65202@gmail.com>
2022-09-27 16:24:46 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,8 +1,8 @@
 function handler(m) {
   
   const kontak = {
-	"displayName": 'My owner',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('6283820073017@s.whatsapp.net')}\nitem1.TEL;waid=6283820073017:6283820073017\nitem1.X-ABLabel:Busy.\nURL;My Web: https://hi.rlxfly.my.id\nEMAIL;Email Owner: me@rlxfly.my.id\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=6287845646738:6287845646738\nEND:VCARD`
+	"displayName": 'Owner Ganteng ku',
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('6287760550924@s.whatsapp.net')}\nitem1.TEL;waid=6287760550924:6287760550924\nitem1.X-ABLabel:Busy.\nURL;My Web: https://hi.LeonOfc202.my.id\nEMAIL;Email Owner: me@twozerot3.my.id\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=6287752078797:6287752078797\nEND:VCARD`
 }
 
 conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
